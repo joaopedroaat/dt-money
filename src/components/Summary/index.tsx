27a6ff8d4abb599ea47f67a8087b588af1b92ec6
joaopedroaat <jotapedro1231@gmail.com>
2023-05-03 16:26:04 -0300

    Created TransactionsContext

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -2,6 +2,8 @@ import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from 'phosphor-react'
 import { SummaryCard, SummaryContainer } from './styles'
 
 export function Summary() {
+  // const { transactions } = useContext(TransactionsContext)
+
   return (
     <SummaryContainer>
       <SummaryCard>
